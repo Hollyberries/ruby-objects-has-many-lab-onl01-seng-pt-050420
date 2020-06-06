5d1786af841279e843a5b8314facbdb3b49d1502
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-06 01:25:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,14 @@
 class Author
   
-  attr_accessor :name
+  attr_accessor :name, :author
   
   def initialize(name)
     @name = name
+    @@author = author
+  end
+  
+  def posts
+    Post.all.select {|post| post.author == self}
   end
   
 end
